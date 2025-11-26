7b9de7d06bd3c417865e6a909edd4609478cd974
waiting2050 <2545670572@qq.com>
2025-11-26 23:39:05 +0800

    增加了user_name的json对应

--- models/user.go
+++ models/user.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	UserName string `gorm:"unique"`
+	UserName string `gorm:"unique" json:"user_name"`
 	PasswordDigest string `json:"password_digest"`
 }
